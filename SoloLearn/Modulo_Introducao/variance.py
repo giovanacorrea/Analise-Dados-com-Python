7336bb9fc6f04d07f142d93ca1bfc2e174a986e9
Giovana Corrêa <giovanacorreadebarros@gmail.com>
2022-08-07 00:22:32 -0300

    Correção do erro de precisão no código de variância

--- SoloLearn/Modulo_Introducao/variance.py
+++ SoloLearn/Modulo_Introducao/variance.py
@@ -1,17 +1,18 @@
 import statistics
+import math
 
 vac_nums = [0,0,0,0,0,1,1,1,1,1,1,1,1,2,2,2,2,3,3,3]
 cont = 0.0
 
 #Calcula a média de vacinações
 for x in vac_nums :
-    if(x>=1) :
-        cont+=x   
-median = cont/20.0
+    cont+=x   
+mean = cont/20.0
 
 #calcula a variancia que é a (média de vacinações - vacinação individual^2)
 variance = 0.0
 for y in vac_nums :
-    variance += ((median-y)**2)/20
+    variance += ((y-mean)**2)/20.0
 
-print(variance)
+print('{:.4f}'.format(variance))
+
