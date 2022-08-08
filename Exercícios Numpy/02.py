741b0c77d304a8be4311e9a9010a1ad818069cb7
Giovana Corrêa <giovanacorreadebarros@gmail.com>
2022-08-08 11:15:32 -0300

    Exercícios Numpy

---
+++ Exercícios Numpy/02.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+#sem arange
+data = np.array([0,1,2,3,4,5,6,7,8,9])
+
+#com arange
+dataA = np.arange(10)
+
+print(data)
+print(dataA)
---
+++ Exercícios Numpy/03.py
@@ -0,0 +1,6 @@
+import numpy as np 
+
+#np.full = Return a new array of given shape and type, filled with fill_value.
+data = np.full((3, 3), True, dtype=bool)
+
+print(data)
